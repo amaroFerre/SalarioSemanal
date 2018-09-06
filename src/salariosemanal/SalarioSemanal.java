323f60742ad656d9a7d3f8bf8ed42cd7b29fed85
Amarico <Amarico@equipo-zalman>
2018-09-06 18:54:08 +0200

    Escribe un programa que calcule el salario semanal de un trabajador teniendo en cuenta que las horas ordinarias ( 40 primeras horas de trabajo) se pagan a 12 euros la hora. A partir de la hora  41, se pagan a 16 euros la hora

---
+++ src/salariosemanal/SalarioSemanal.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package salariosemanal;
+
+/**
+ * Escribe un programa que calcule el salario semanal de un trabajador teniendo
+ * en cuenta que las horas ordinarias (40 primeras horas de trabajo) se pagan a
+ * 12 euros la hora. A partir de la hora 41, se pagan a 16 euros la hora
+ *
+ * @author Amarico
+ */
+public class SalarioSemanal {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO code application logic here
+  }
+  
+}
